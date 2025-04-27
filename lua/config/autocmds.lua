ef5a48a9772e2a733313d23f906b08aa1c93a867
mikmikinmoko <mikmikinmoko05@gmail.com>
2025-04-27 20:39:19 +0800

    updated nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Highlighting text when yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("YankHighLight", {
+    clear = true,
+  }),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+  end,
+  desc = "Highlight yank",
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
+
+vim.keymap.set("n", "<C-d", "5j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u", "5k", { noremap = true, silent = true })
